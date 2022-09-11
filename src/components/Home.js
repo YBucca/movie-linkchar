561fc95e5ba3ebb1a3f509970b023092c5afdd03
YBucca <yani.bucca@gmail.com>
2022-09-11 10:10:21 -0300

    fix breakpoint

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,13 @@ import ContainerPopular from "./ContainerPopular";
 import ContainerContinue from "./ContainerContinue";
 import Banner from "./Banner";
 import Aside from "./Aside";
+import "../App.css"
 
 const Home = () => {
 	return (
 		<div className="d-flex">
 			<Aside />
-			<div className="container d-flex flex-column">
+			<div className="container d-flex flex-column container-break">
 				<Banner />
 				<ContainerContinue />
 				<ContainerPopular />
